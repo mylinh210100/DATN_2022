9be1f714b749059b7251a70147ad230c806aa176
DoanLeMyLinh <mylinh210100@gmail.com>
2022-06-18 14:36:53 +0700

    commit 1806 2

--- predict_stockprice_LSTM.py
+++ predict_stockprice_LSTM.py
@@ -18,6 +18,7 @@ from keras.layers import Dense, LSTM, Dropout
 df = data.DataReader('TSLA', data_source='yahoo', start ='2014-01-01', end='2021-12-30')
 print(df)
 
+"""
     #mo hinh hoa data 'close' day
 plt.figure(figsize=(12,6))
 plt.title('Close Price History of TSLA')
@@ -34,7 +35,7 @@ plt.xlabel('Time', fontsize=18)
 plt.ylabel('Volume', fontsize=18)
 plt.show()
 
-"""
+
     # We'll use pct_change to find the percent change for each day
 df['Daily return'] = df['Close'].pct_change()
 #print(df['Daily return'])
@@ -53,23 +54,27 @@ dataf = df.filter(['Close'])
     # Convert DF to a numpy array
 dataset = dataf.values
 
-    # Get the number of rows to train the model on 85%
+    # Get the number to train the model on 85%
 training_data_len = int(np.ceil( len(dataset) * 0.85 ))
-#print(training_data_len) #1612
+#print(training_data_len) #1731
+
+
 
     # Scale the data 
-    # In our case, we’ll use Scikit- Learn’s MinMaxScaler and scale our dataset to numbers between zero and one.
+    # use Scikit- Learn’s MinMaxScaler and scale our dataset to numbers between zero and one.
 scaler = MinMaxScaler(feature_range=(0,1))
 scaled_data = scaler.fit_transform(dataset)
 print(scaled_data)
 
-# Create the training data set 
-# Create the scaled training data set
-train_data = scaled_data[0:training_data_len, :]
-# Split the data into x_train and y_train data sets
+
+    # tao dataset de training  
+    # Create the scaled training data set
+train_data = scaled_data[0:training_data_len, :] #get data from 0 to 1731, and all column
+    # Split the data into x_train and y_train datasets
 x_train = []
 y_train = []
 
+    #Creating a data structure with 60 time-steps and 1 output
 for i in range(60, len(train_data)):
     x_train.append(train_data[i-60:i, 0])
     y_train.append(train_data[i, 0])
@@ -78,63 +83,72 @@ for i in range(60, len(train_data)):
         print(y_train)
         print()
 
-# Convert the x_train and y_train to numpy arrays 
+    # Convert the x_train and y_train to numpy arrays 
 x_train, y_train = np.array(x_train), np.array(y_train)
 
-# Reshape the data
+    # Reshape the data
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+#print(x_train.shape) (1653,60,1)
 
-# Build the LSTM model 1st layer
+
+    # Build the LSTM model
 model = Sequential()
-model.add(LSTM(50, return_sequences=True, input_shape=(x_train.shape[1], 1))) # 50 layer
-
-
-
+    # 1st layer
+model.add(LSTM(128, return_sequences=True, input_shape=(x_train.shape[1], 1))) # 50 neuron
+    # 2nd layer
+model.add(LSTM(64, return_sequences=False))
 """
-# 2nd layer
-model.add(LSTM(50, return_sequences=True))
+    # 3rd layer
+model.add(LSTM(50, return_sequences= True))
 model.add(Dropout(0.2))
-"""
-"""
+    # 4th layer
+model.add(LSTM(50))
+model.add(Dropout(0.2))
 
-# output layer
+    #output layer
+model.add(Dense(1))
+"""
+    # output layer
 model.add(Dense(25)) #25 noron
-model.add(Dense(units=1))
+model.add(Dense(1))
 
-# Compile the model
+    # Compile the RNN
 model.compile(optimizer='adam', loss='mean_squared_error')
 
-# Train the model
-model.fit(x_train, y_train, batch_size=1, epochs=1)
+    # Fitting the RNN to Train the model
+model.fit(x_train, y_train, batch_size=1, epochs=2)
 
-# Create the testing data set
-# Create a new array containing scaled values from  to
+    # Create the testing data set
+    #Create a new array containing scaled values from  to
 test_data = scaled_data[training_data_len - 60: , :]
-# Create the data sets x_test and y_test
+
+    # Create the data sets x_test and y_test
 x_test = []
 y_test = dataset[training_data_len:, :]
 for i in range(60, len(test_data)):
     x_test.append(test_data[i-60:i, 0])
     
-# Convert the data to a numpy array
+    # Convert the data to a numpy array
 x_test = np.array(x_test)
 
-# Reshape the data
+    # Reshape the data
 x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1 ))
+print(x_test.shape) #(302,60,1)
 
-# Get the models predicted price values 
+    # Get the models predicted price values 
 predictions = model.predict(x_test)
 predictions = scaler.inverse_transform(predictions)
 
-# Get the root mean squared error (RMSE)
+    # Get the root mean squared error (RMSE) loi trung binh binh phuong goc
 rmse = np.sqrt(np.mean(((predictions - y_test) ** 2)))
 print(rmse)
 
-# Plot the data
+    # Plot the data - ve du lieu
 train = dataf[:training_data_len]
 valid = dataf[training_data_len:]
 valid['Predictions'] = predictions
-# Visualize the data
+
+    # Visualize the data
 plt.figure(figsize=(16,8))
 plt.title('Tesla Stock price prediction')
 plt.xlabel('Date', fontsize=18)
@@ -144,13 +158,13 @@ plt.plot(valid[['Close', 'Predictions']])
 plt.legend(['Train', 'Real stock price', 'Predictions stock price'], loc='lower right')
 plt.show()
 
-# Show the valid and predicted prices
-print(valid)
+    # Show the valid and predicted prices
+#print(valid)
+
 
 
 """
-
-"""#get the 'close' quote 
+#get the 'close' quote 
 
 tesla_quote = data.DataReader('TSLA', data_source='yahoo', start='2022-01-01', end='2022-05-01')
 
@@ -173,5 +187,5 @@ pred_price = model.predict(X_test)
 pred_price = scaler.inverse_transform(pred_price)
 
 print(pred_price)
-"""
 
+"""
